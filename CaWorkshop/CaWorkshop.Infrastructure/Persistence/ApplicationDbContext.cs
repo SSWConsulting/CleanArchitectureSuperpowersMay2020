b9fe5172fe136853fbb711fa963dd2f3f2cb2c5b
Jason Taylor <jasongt@outlook.com>
2020-05-29 11:22:39 +1000

    Beyond-Basics-1

---
+++ CaWorkshop/CaWorkshop.Infrastructure/Persistence/ApplicationDbContext.cs
@@ -0,0 +1,31 @@
+﻿using CaWorkshop.Domain.Entities;
+using CaWorkshop.Infrastructure.Identity;
+using CaWorkshop.Infrastructure.Persistence.Configurations;
+using IdentityServer4.EntityFramework.Options;
+using Microsoft.AspNetCore.ApiAuthorization.IdentityServer;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+using System.Reflection;
+
+namespace CaWorkshop.Infrastructure.Persistence
+{
+    public class ApplicationDbContext : ApiAuthorizationDbContext<ApplicationUser>
+    {
+        public ApplicationDbContext(
+            DbContextOptions options,
+            IOptions<OperationalStoreOptions> operationalStoreOptions) : base(options, operationalStoreOptions)
+        {
+        }
+
+        public DbSet<TodoItem> TodoItems { get; set; }
+
+        public DbSet<TodoList> TodoLists { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            builder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+
+            base.OnModelCreating(builder);
+        }
+    }
+}
---
+++ CaWorkshop/CaWorkshop.Infrastructure/Persistence/ApplicationDbContextSeeder.cs
@@ -0,0 +1,32 @@
+﻿using CaWorkshop.Domain.Entities;
+using System.Linq;
+
+namespace CaWorkshop.Infrastructure.Persistence
+{
+    public static class ApplicationDbContextSeeder
+    {
+        public static void Seed(ApplicationDbContext context)
+        {
+            if (context.TodoLists.Any())
+            {
+                return;
+            }
+
+            var list = new TodoList
+            {
+                Title = "Death List Five",
+                Items =
+                {
+                    new TodoItem { Title = "O-Ren Ishii", Done = true },
+                    new TodoItem { Title = "Vernita Green", Done = true },
+                    new TodoItem { Title = "Budd", Done = true },
+                    new TodoItem { Title = "Ellie Driver" },
+                    new TodoItem { Title = "Bill" }
+                }
+            };
+
+            context.TodoLists.Add(list);
+            context.SaveChanges();
+        }
+    }
+}
