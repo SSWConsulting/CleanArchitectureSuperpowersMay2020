f049126d5a7861bb0009aaa05d57f27ddddf0146
Jason Taylor <jasongt@outlook.com>
2020-05-29 16:58:42 +1000

    Advanced-Topics-2

---
+++ CaWorkshop/CaWorkshop.Application/Common/Behaviours/UnhandledExceptionBehaviour.cs
@@ -0,0 +1,34 @@
+﻿using MediatR;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace CaWorkshop.Application.Common.Behaviours
+{
+    // TODO: Review https://jimmybogard.com/mediatr-8-0-released/ (includes IRequestExceptionHandler)
+    public class UnhandledExceptionBehaviour<TRequest, TResponse> : IPipelineBehavior<TRequest, TResponse>
+    {
+        private readonly ILogger<TRequest> _logger;
+
+        public UnhandledExceptionBehaviour(ILogger<TRequest> logger)
+        {
+            _logger = logger;
+        }
+
+        public async Task<TResponse> Handle(TRequest request, CancellationToken cancellationToken, RequestHandlerDelegate<TResponse> next)
+        {
+            try
+            {
+                return await next();
+            }
+            catch (Exception ex)
+            {
+                var requestName = typeof(TRequest).Name;
+
+                _logger.LogError(ex, "CaWorkshop Request: Unhandled Exception for Request {Name} {@Request}", requestName, request);
+
+                throw;
+            }
+        }
+    }
