f124613e062af0cf43d190960460f8c4d5d63d37
Jason Taylor <jasongt@outlook.com>
2020-05-29 16:33:55 +1000

    Beyond-Basics-8

--- CaWorkshop/CaWorkshop.Application.UnitTests/DbContextFactory.cs
+++ CaWorkshop/CaWorkshop.Application.UnitTests/DbContextFactory.cs
@@ -1,7 +1,9 @@
-﻿using CaWorkshop.Infrastructure.Persistence;
+﻿using CaWorkshop.Application.Common.Interfaces;
+using CaWorkshop.Infrastructure.Persistence;
 using IdentityServer4.EntityFramework.Options;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Options;
+using Moq;
 using System;
 
 namespace CaWorkshop.Application.UnitTests
@@ -11,7 +13,7 @@ namespace CaWorkshop.Application.UnitTests
         public static ApplicationDbContext Create()
         {
             var options = new DbContextOptionsBuilder<ApplicationDbContext>()
-                .UseInMemoryDatabase(databaseName: Guid.NewGuid().ToString())
+                .UseInMemoryDatabase(Guid.NewGuid().ToString())
                 .Options;
 
             var operationalStoreOptions = Options.Create(
@@ -23,7 +25,13 @@ namespace CaWorkshop.Application.UnitTests
                         new TableConfiguration("PersistedGrants")
                 });
 
-            var context = new ApplicationDbContext(options, operationalStoreOptions);
+            var currentUserServiceMock = new Mock<ICurrentUserService>();
+            currentUserServiceMock.Setup(m => m.UserId)
+                .Returns("00000000-0000-0000-0000-000000000000");
+
+            var context = new ApplicationDbContext(
+                options, operationalStoreOptions,
+                currentUserServiceMock.Object);
 
             ApplicationDbContextSeeder.Seed(context);
 
